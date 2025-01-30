96f8ba6c1f3079ce7bd532b30b8ab6318293862a
fasdamc@gmail.com <fasdamc@gmail.com>
2025-01-30 13:53:52 -0500

    version 1

--- src/ex_JavaBasics/lab_variables.java
+++ src/ex_JavaBasics/lab_variables.java
@@ -3,5 +3,9 @@ package ex_JavaBasics;
 public class lab_variables {
     public static void main(String[] args) {
         System.out.println("First commit");
+        int age = 10;
+        System.out.println(age);
+        age = 10 + 5;
+        System.out.println(age);
     }
 }
