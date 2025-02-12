56e20aa583cb3996c2cbd54121121add4586e9af
fasika-d-chefik_DFIN <fasika.d.chefik@dfinsolutions.com>
2025-02-11 23:35:20 -0500

    lab_ternary

---
+++ src/ex_JavaBasics/lab_jan19_ternary.java
@@ -0,0 +1,12 @@
+package ex_JavaBasics;
+
+public class lab_jan19_ternary {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 45;
+        int c = 60;
+
+        int largest = (a > b && a > c) ? a : (b > c) ? b : c;
+        System.out.println(largest);
+    }
+}
